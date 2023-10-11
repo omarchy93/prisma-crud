201fd6a71c60fa33fd8cf6ec478dbb4896a67576
omar <omarfarukchy93@gmail.com>
2023-10-11 23:53:07 +0600

    crud

---
+++ prisma/migrations/20231011172607_user/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `firstName` VARCHAR(50) NOT NULL,
+    `middleName` VARCHAR(50) NOT NULL,
+    `lastName` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `mobile` VARCHAR(15) NOT NULL,
+    `password` VARCHAR(32) NOT NULL,
+    `registeredAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `lastLogin` TIMESTAMP(0) NOT NULL,
+    `intro` VARCHAR(200) NOT NULL,
+    `profile` VARCHAR(200) NOT NULL,
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
