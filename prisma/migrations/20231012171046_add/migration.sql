6ffc1f2b1f6aacca1bc3a14b1c40888f5b606356
omar <omarfarukchy93@gmail.com>
2023-10-12 23:33:32 +0600

    add

--- prisma/migrations/20231012171046_add/migration.sql
+++ prisma/migrations/20231012171046_add/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `user`;
-
 -- CreateTable
 CREATE TABLE `users` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -26,10 +17,8 @@ CREATE TABLE `users` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `post` (
+CREATE TABLE `posts` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `authorId` BIGINT NOT NULL,
-    `parentId` BIGINT NOT NULL,
     `title` VARCHAR(75) NOT NULL,
     `metaTitle` VARCHAR(100) NOT NULL,
     `slug` VARCHAR(100) NOT NULL,
@@ -38,11 +27,27 @@ CREATE TABLE `post` (
     `content` VARCHAR(200) NOT NULL,
     `userId` BIGINT UNSIGNED NOT NULL,
     `createAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-    `updateAt` TIMESTAMP(0) NOT NULL,
+    `updateAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `publishedAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `postComments` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(100) NOT NULL,
+    `content` VARCHAR(200) NOT NULL,
+    `published` TINYINT NOT NULL,
+    `postId` BIGINT UNSIGNED NOT NULL,
+    `createAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
     `publishedAt` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `post` ADD CONSTRAINT `post_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `posts` ADD CONSTRAINT `posts_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `postComments` ADD CONSTRAINT `postComments_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `posts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/app/api/tags/route.js
@@ -0,0 +1,74 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function POST(req, res) {
+  BigInt.prototype.toJSON = function () {
+    return this.toString();
+  };
+  try {
+    const prisma = new PrismaClient();
+    let reqBody = await req.json();
+    let result = await prisma.users.createMany({
+      data: reqBody,
+    });
+    return NextResponse.json({ status: "success", result: result });
+  } catch (error) {
+    return NextResponse.json({ status: "fail", result: error.toString() });
+  }
+}
+//find many
+export async function GET(req, res) {
+  BigInt.prototype.toJSON = function () {
+    return this.toString();
+  };
+  try {
+    const prisma = new PrismaClient();
+
+    let result = await prisma.users.findMany();
+    return NextResponse.json({ status: "success", result: result });
+  } catch (error) {
+    return NextResponse.json({ status: "fail", result: error.toString() });
+  }
+}
+// update
+export async function PUT(req, res) {
+  BigInt.prototype.toJSON = function () {
+    return this.toString();
+  };
+  try {
+    const prisma = new PrismaClient();
+    let reqBody = await req.json();
+
+    let result = await prisma.users.update({
+      where: reqBody,
+      data: {
+        title: "ccccc",
+        metaTitle: "gccc",
+        slug: "chyasdfasdfccd",
+        content: "12fdf3fd232",
+      },
+    });
+    return NextResponse.json({ status: "success", result: result });
+  } catch (error) {
+    return NextResponse.json({ status: "fail", result: error.toString() });
+  }
+}
+
+// delete
+
+export async function DELETE(req, res) {
+  BigInt.prototype.toJSON = function () {
+    return this.toString();
+  };
+  try {
+    const prisma = new PrismaClient();
+    let reqBody = await req.json();
+
+    let result = await prisma.users.delete({
+      where: reqBody,
+    });
+    return NextResponse.json({ status: "success", result: result });
+  } catch (error) {
+    return NextResponse.json({ status: "fail", result: error.toString() });
+  }
+}
